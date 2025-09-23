592aa853025c2dd11e1aef60ae13f009641e2c4e
Amanuel <amanuelteshale083@gmail.com>
2025-09-23 09:37:53 +0300

    change tsconfig settigns

--- auth-service/src/modules/PermissionModule/permission.controller.ts
+++ auth-service/src/modules/PermissionModule/permission.controller.ts
@@ -4,31 +4,33 @@ import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
 import { RolesGuard } from '../../common/guards/roles.guard';
 import { PermissionGuard } from '../../common/guards/permission.guard';
 import { Permissions } from '../../common/decorators/permission.decorator';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
 
 
 @Controller('permissions')
-@UseGuards(JwtAuthGuard, RolesGuard, PermissionGuard)
+@UseGuards(JwtAuthGuard, PermissionGuard)
+
 export class PermissionsController {
 constructor(private readonly permissionsService: PermissionsService) {}
 
 
 @Get()
-@Permissions('permissions:read')
+@ApiBearerAuth('access-token')
+@Permissions('permissions.manage')
 findAll() {
 return this.permissionsService.findAll();
 }
 
 
 @Get(':id')
-@Permissions('permissions:read')
+@Permissions('permission.manage')
 findById(@Param('id') id: string) {
 return this.permissionsService.findById(id);
 }
 
-
 @Post()
-@Permissions('permissions:create')
+@Permissions('permissions.manage')
 createPermission(@Body('name') name: string, @Body('description') description?: string) {
 return this.permissionsService.createPermission(name, description);
-}
+ }
 }
